d1f93f54826f517fa5393e54fd3e1c0fc77d02ba
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2022-06-19 21:33:22 +0100

    Added all folders/files

---
+++ Class 1/script.js
@@ -0,0 +1,28 @@
+
+function add () {
+    var firstInp = firstNum.value;
+    var secondInp = secondNum.value;
+    var total = Number(firstInp) + Number(secondInp);
+    result.innerText = total;
+}
+
+function subt () {
+    var firstInp = firstNum.value;
+    var secondInp = secondNum.value;
+    var total = Number(firstInp) - Number(secondInp);
+    result.innerText = total;
+}
+
+function div() {
+    var firstInp = firstNum.value;
+    var secondInp = secondNum.value;
+    var total = Number(firstInp) / Number(secondInp);
+    result.innerText = total;
+}
+
+function mul() {
+    var firstInp = firstNum.value;
+    var secondInp = secondNum.value;
+    var total = Number(firstInp) * Number(secondInp);
+    result.innerText = total;
+}
